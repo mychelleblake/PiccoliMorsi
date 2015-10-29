5aff8c4846929c0ab1402f80c261aa524fc4089e
notewrangler <noiseartist@gmail.com>
2015-10-29 15:24:27 -0700

    added the about text

--- main.js
+++ main.js
@@ -14,48 +14,60 @@ $(document).ready (function() {
 			var newsDataPull = {
 				newsBoxData : newsBoxData
 			};
-
-		var newsFormTemplate = $("#newsFormTemplate").text();   //CHANGE THE NAME OF THE ID BASED ON HTML
-		var formHTML = Mustache.render(newsFormTemplate, newsDataPull);
-		$("#theForm").html(formHTML);  //CHANGE THE NAME OF THE ID BASED ON HTML
+			console.log(newsDataPull);
+		// var newsFormTemplate = $("#newsFormTemplate").text();   //CHANGE THE NAME OF THE ID BASED ON HTML
+		// var formHTML = Mustache.render(newsFormTemplate, newsDataPull);
+		// $("#theForm").html(formHTML);  //CHANGE THE NAME OF THE ID BASED ON HTML
 	});
 
 	// retrieve specials data
 
-		var specialsData = "https://json-data.herokuapp.com/restaurant/special/1";
 
-		$.ajax({
-			url: specialsData,
-			method: 'get',
-			dataType: 'json'
-		}).then(function (specialsBoxData) {
+			var menuData = "http://79.170.44.100/piccolimorsi.com/menu.json";
 
-		var specialsDataPull = {
-				specialsBoxData : specialBoxData
-			};
+			$.ajax({
+				url: menuData,
+				method: 'get',
+				dataType: 'json'
+			}).then(function (menuBoxData) {
 
-		var specialsFormTemplate = $("#specialsFormTemplate").text();   //CHANGE THE NAME OF THE ID BASED ON HTML
-		var formHTML = Mustache.render(specialsFormTemplate, specialsDataPull);
-		$("#theForm").html(formHTML);  //CHANGE THE NAME OF THE ID BASED ON HTML
-	});
+				var menuDataPull = {
+					menuBoxData : menuBoxData
+				};
+
+				console.log(menuDataPull);
+
+				var specialsData = "https://json-data.herokuapp.com/restaurant/special/1";
+
+				var spec = function() {
+					$.ajax({
+						url: specialsData,
+						method: 'get',
+						dataType: 'json'
+					}).then(function (specialsBoxData) {
+
+						var specialsDataPull = {
+								specialsBoxData : specialsBoxData
+							};
+
+					console.log(specialsDataPull);
+
+
+			// var menuFormTemplate = $("#menuFormTemplate").text();   //CHANGE THE NAME OF THE ID BASED ON HTML
+			// var formHTML = Mustache.render(specialsFormTemplate,specialsDataPull);
+			// $("#theForm").html(formHTML);  //CHANGE THE NAME OF THE ID BASED ON HTML
+
+			// var specialsFormTemplate = $("#specialsFormTemplate").text();   //CHANGE THE NAME OF THE ID BASED ON HTML
+			// var formHTML = Mustache.render(specialsFormTemplate, specialsDataPull);
+			// $("#theForm").html(formHTML);  //CHANGE THE NAME OF THE ID BASED ON HTML
+
+		});
+
+		});
+
+	};
 
 // retreive menu data from server
 
-		var menuData = "http://79.170.44.100/piccolimorsi.com/menu.json";
-
-		$.ajax({
-			url: menuData,
-			method: 'get',
-			dataType: 'json'
-		}).then(function (menuBoxData) {
-
-			var menuDataPull = {
-				menuBoxData : menuBoxData
-			};
-
-		var menuFormTemplate = $("#menuFormTemplate").text();   //CHANGE THE NAME OF THE ID BASED ON HTML
-		var formHTML = Mustache.render(specialsFormTemplate,specialsDataPull);
-		$("#theForm").html(formHTML);  //CHANGE THE NAME OF THE ID BASED ON HTML
-	});
 
 });
